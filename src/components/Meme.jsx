38bb24bf0b5a2c28299deaa7829cf0dce25e4889
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-24 11:42:39 +0200

    add api call

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const Meme = () => {
+
+    const [memes, setMemes] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://api.imgflip.com/get_memes')
+            .then(response => {
+                setMemes(response.data.memes)
+                console.log(response.data.data.memes);
+            })
+            .catch(error =>
+                console.log(error))
+    })
     return (
         <div>
             <div className="container">
-                <form className="meme_form row mt-4 gap-3 justify-content-center" action="">
+                <form className="meme_form row mt-5 gap-3 justify-content-center" action="">
 
                     <input type="text" id='top_text' className='meme_input col-5 rounded py-2' placeholder='Top Text' />
                     <input type="text" id='bottom_text' className='meme_input col-5 rounded py-2' placeholder='Bottom Text' />

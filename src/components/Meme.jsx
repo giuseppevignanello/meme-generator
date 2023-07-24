27b4235f911231ef9e445cba0145ddac5b5418a1
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-24 12:05:57 +0200

    add images dynamically

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,16 +4,23 @@ import axios from 'axios';
 const Meme = () => {
 
     const [memes, setMemes] = useState([]);
+    const [imagePath, setImagePath] = useState('')
 
     useEffect(() => {
         axios.get('https://api.imgflip.com/get_memes')
             .then(response => {
-                setMemes(response.data.memes)
-                console.log(response.data.data.memes);
+                setMemes(response.data.data.memes)
+                const randomNumber = Math.floor(Math.random() * memes.length)
+                setImagePath(memes[randomNumber].url)
             })
             .catch(error =>
                 console.log(error))
-    })
+    }, []);
+
+    function getRandomMeme() {
+        const randomNumber = Math.floor(Math.random() * memes.length)
+        setImagePath(memes[randomNumber].url)
+    }
     return (
         <div>
             <div className="container">
@@ -22,9 +29,12 @@ const Meme = () => {
                     <input type="text" id='top_text' className='meme_input col-5 rounded py-2' placeholder='Top Text' />
                     <input type="text" id='bottom_text' className='meme_input col-5 rounded py-2' placeholder='Bottom Text' />
 
-                    <button type="button" name="" id="" className="btn btn-success form_btn col-5">Get a new image</button>
+                    <button onClick={getRandomMeme} type="button" name="" id="" className="btn btn-success form_btn col-5">Get a new image</button>
 
                 </form>
+                <div className="image_meme text-center mt-5">
+                    <img src={imagePath} alt="" />
+                </div>
             </div>
         </div>
     )

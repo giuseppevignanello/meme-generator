5d6c013680068a4d956ffad84e18df5f46c7d5fb
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-25 12:40:53 +0200

    refactoring

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -5,8 +5,7 @@ const Meme = () => {
 
     const [memes, setMemes] = useState([]);
     const [imagePath, setImagePath] = useState('')
-    const [topText, setTopText] = useState('')
-    const [bottomText, setBottomText] = useState('')
+    const [textData, setTextData] = useState({ topText: "", bottomText: "" })
 
 
     useEffect(() => {
@@ -25,21 +24,24 @@ const Meme = () => {
         setImagePath(memes[randomNumber].url)
     }
 
-    function handleChangeTop(event) {
-        setTopText(event.target.value)
+    function handleTextChange(event) {
+        setTextData(prevTextData => {
+            return {
+                ...prevTextData,
+                [event.target.name]: event.target.value
+            }
+        })
     }
 
-    function handleChangeBottom(event) {
-        setBottomText(event.target.value)
-    }
+
 
     return (
         <div>
             <div className="container">
                 <form className="meme_form row mt-5 gap-3 justify-content-center" action="">
 
-                    <input onChange={handleChangeTop} type="text" id='top_text' className='meme_input col-5 rounded py-2' placeholder='Top Text' />
-                    <input onChange={handleChangeBottom} type="text" id='bottom_text' className='meme_input col-5 rounded py-2' placeholder='Bottom Text' />
+                    <input onChange={handleTextChange} name='topText' type="text" id='top_text' className='meme_input col-5 rounded py-2' placeholder='Top Text' />
+                    <input onChange={handleTextChange} name='bottomText' type="text" id='bottom_text' className='meme_input col-5 rounded py-2' placeholder='Bottom Text' />
 
                     <button onClick={getRandomMeme} type="button" name="" id="" className="bg_main form_btn col-5 p-2 rounded border-1">Get a new image</button>
 
@@ -47,8 +49,8 @@ const Meme = () => {
                 <div className="image_meme text-center mt-5 position-relative">
                     <img src={imagePath} alt="" />
                     <div className='text_container d-flex flex-column justify-content-between h-100'>
-                        <p className='text' >{topText}</p>
-                        <p className='text'>{bottomText}</p>
+                        <p className='text' >{textData.topText}</p>
+                        <p className='text'>{textData.bottomText}</p>
                     </div>
                 </div>
 

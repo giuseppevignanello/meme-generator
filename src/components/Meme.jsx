4522921d728c57dec5a9b8698306cd5df45f5466
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-25 12:13:30 +0200

    add onChange functions

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -5,6 +5,9 @@ const Meme = () => {
 
     const [memes, setMemes] = useState([]);
     const [imagePath, setImagePath] = useState('')
+    const [topText, setTopText] = useState('')
+    const [bottomText, setBottomText] = useState('')
+
 
     useEffect(() => {
         axios.get('https://api.imgflip.com/get_memes')
@@ -21,13 +24,22 @@ const Meme = () => {
         const randomNumber = Math.floor(Math.random() * memes.length)
         setImagePath(memes[randomNumber].url)
     }
+
+    function handleChangeTop(event) {
+        setTopText(event.target.value)
+    }
+
+    function handleChangeBottom(event) {
+        setBottomText(event.target.value)
+    }
+
     return (
         <div>
             <div className="container">
                 <form className="meme_form row mt-5 gap-3 justify-content-center" action="">
 
-                    <input type="text" id='top_text' className='meme_input col-5 rounded py-2' placeholder='Top Text' />
-                    <input type="text" id='bottom_text' className='meme_input col-5 rounded py-2' placeholder='Bottom Text' />
+                    <input onChange={handleChangeTop} type="text" id='top_text' className='meme_input col-5 rounded py-2' placeholder='Top Text' />
+                    <input onChange={handleChangeBottom} type="text" id='bottom_text' className='meme_input col-5 rounded py-2' placeholder='Bottom Text' />
 
                     <button onClick={getRandomMeme} type="button" name="" id="" className="bg_main form_btn col-5 p-2 rounded border-1">Get a new image</button>
 
@@ -35,6 +47,9 @@ const Meme = () => {
                 <div className="image_meme text-center mt-5">
                     <img src={imagePath} alt="" />
                 </div>
+                <p>{topText}</p>
+                <p>{bottomText}</p>
+
             </div>
         </div>
     )

446684e3968fdb4e25865a38a047828db3d71837
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-25 15:48:53 +0200

    add increase and decrease font size function

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -6,6 +6,10 @@ const Meme = () => {
     const [memes, setMemes] = useState([]);
     const [imagePath, setImagePath] = useState('')
     const [textData, setTextData] = useState({ topText: "", bottomText: "" })
+    const [fontSizeTop, setFontSizeTop] = useState(20)
+    const [topSelected, setTopSelected] = useState(false)
+    const [bottomSelected, setBottomSelected] = useState(false)
+    const [fontSizeBottom, setFontSizeBottom] = useState(20)
 
 
     useEffect(() => {
@@ -33,6 +37,31 @@ const Meme = () => {
         })
     }
 
+    function selectTop() {
+        setTopSelected(prevData => !prevData)
+    }
+
+    function selectBottom() {
+        setBottomSelected(prevData => !prevData)
+    }
+
+    function increaseFontSizeTop() {
+        setFontSizeTop(prevData => prevData + 2)
+    }
+
+    function decreaseFontSizeTop() {
+        setFontSizeTop(prevData => prevData - 2)
+    }
+
+    function increaseFontSizeBottom() {
+        setFontSizeBottom(prevData => prevData + 2)
+    }
+
+    function decreaseFontSizeBottom() {
+        setFontSizeBottom(prevData => prevData - 2)
+    }
+
+
 
 
     return (
@@ -40,17 +69,44 @@ const Meme = () => {
             <div className="container">
                 <form className="meme_form row mt-5 gap-3 justify-content-center" action="">
 
-                    <input onChange={handleTextChange} name='topText' type="text" id='top_text' className='meme_input col-5 rounded py-2' placeholder='Top Text' />
-                    <input onChange={handleTextChange} name='bottomText' type="text" id='bottom_text' className='meme_input col-5 rounded py-2' placeholder='Bottom Text' />
+                    <input onChange={handleTextChange} value={textData.topText} name='topText' type="text" id='top_text' className='meme_input col-5 rounded py-2' placeholder='Top Text' />
+                    <input onChange={handleTextChange} value={textData.bottomText} name='bottomText' type="text" id='bottom_text' className='meme_input col-5 rounded py-2' placeholder='Bottom Text' />
 
                     <button onClick={getRandomMeme} type="button" name="" id="" className="bg_main form_btn col-5 p-2 rounded border-1">Get a new image</button>
 
                 </form>
                 <div className="image_meme text-center mt-5 position-relative">
                     <img src={imagePath} alt="" />
+
+
+                    {/* text */}
                     <div className='text_container d-flex flex-column justify-content-between h-100'>
-                        <p className='text' >{textData.topText}</p>
-                        <p className='text'>{textData.bottomText}</p>
+                        <p onClick={selectTop} style={{ fontSize: `${fontSizeTop}px` }} className='text' >{textData.topText.toUpperCase()}</p>
+                        <p onClick={selectBottom} style={{ fontSize: `${fontSizeBottom}px` }} className='text'>{textData.bottomText.toUpperCase()}</p>
+                    </div>
+                </div>
+
+
+                {/* buttons top */}
+                <div className={topSelected ? "d-block" : "d-none"}>
+                    <div>
+                        <h6 className='text-center mt-2'>Font-size Top Text</h6>
+                    </div>
+                    <div className='d-flex justify-content-center mt-3 gap-3'>
+                        <button onClick={increaseFontSizeTop} type="button" className="btn btn-secondary">⬆️</button>
+                        <button onClick={decreaseFontSizeTop} type="button" className="btn btn-secondary">⬇️</button>
+                    </div>
+                </div>
+
+
+                {/* buttons bottom */}
+                <div className={bottomSelected ? "d-block" : "d-none"}>
+                    <div>
+                        <h6 className='text-center mt-2'>Font-size Bottom Text</h6>
+                    </div>
+                    <div className=' d-flex justify-content-center mt-3 gap-3'>
+                        <button onClick={increaseFontSizeBottom} type="button" className="btn btn-secondary">⬆️</button>
+                        <button onClick={decreaseFontSizeBottom} type="button" className="btn btn-secondary">⬇️</button>
                     </div>
                 </div>
 
